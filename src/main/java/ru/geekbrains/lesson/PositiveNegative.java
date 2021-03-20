e1186e20da29a8667af9deaf7a00b642379f9dee
AndreyD1996 <andreydoronin96@mail.ru>
2021-03-20 15:57:57 +0300

    Lesson1

--- src/main/java/ru/geekbrains/lesson/PositiveNegative.java
+++ src/main/java/ru/geekbrains/lesson/PositiveNegative.java
@@ -1,2 +1,13 @@
-package ru.geekbrains.lesson;public class PositiveNegative {
+
+package ru.geekbrains.lesson;
+
+public class PositiveNegative {public static void main(String[] args) {
+    int a = 0;
+    if (a >= 0 ) {
+        System.out.println("с положительное");
+
+    } else {
+        System.out.println("c отрицательное");
+    }
+}
 }
